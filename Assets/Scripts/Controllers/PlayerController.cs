c85ac8abe1b2eda730ef698006c3f81c9210a059
Admin <mjrahey@mun.ca>
2021-02-22 11:15:04 -0330

    added esc to quit

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -83,5 +83,10 @@ public class PlayerController : MonoBehaviour
                 }
             }
         }
+
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 }
