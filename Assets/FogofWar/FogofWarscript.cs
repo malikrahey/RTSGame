4a09827f1933fdf281b7df31efd9bbdf250cd787
Njtwells <77252947+Njtwells@users.noreply.github.com>
2021-03-19 17:41:18 -0230

    fog of war
    Made it so that the fog of war will start out all black

---
+++ Assets/FogofWar/FogofWarscript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogofWarscript : MonoBehaviour
+{
+    public GameObject FogofWarPlane;
+    public Transform units;
+    public LayerMask FogLayer;
+    public float radius = 5f;
+    private float radiusSquared { get { return radius * radius; } }
+
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private Color[] colours;
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Initialize()
+    {
+        mesh = FogofWarPlane.GetComponent<MeshFilter>().mesh;
+        vertices = mesh.vertices;
+        colours = new Color[vertices.Length];
+        for(int i = 0; i < colours.Length; i++)
+        {
+            colours[i] = Color.black;
+        }
+        UpdateColours();
+    }
+
+    void UpdateColours()
+    {
+        mesh.colors = colours;
+    }
+}
