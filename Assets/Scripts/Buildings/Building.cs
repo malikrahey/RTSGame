c575eaea2c6c66c58006d47f2af2928909f90399
Njtwells <77252947+Njtwells@users.noreply.github.com>
2021-04-01 12:48:38 -0230

    Added health bar
    Added a health bar to the factory, but I can't attack it to see if it works

--- Assets/Scripts/Buildings/Building.cs
+++ Assets/Scripts/Buildings/Building.cs
@@ -5,8 +5,23 @@ using UnityEngine;
 public class Building : MonoBehaviour
 {
     public float Health { get; set; }
+    private HealthBar healthbar;
 
 
+    private void Start()
+    {
+        healthbar = this.gameObject.GetComponentInChildren<HealthBar>();
+        if(healthbar == null)
+        {
+            GameObject healthBarGO = Instantiate(GameManager.Instance.healthBarPrefab) as GameObject;
+            healthBarGO.transform.position = new Vector3(50, 50, -2);
+            healthBarGO.transform.rotation = Quaternion.Euler(-45, 180, 0);
+            healthBarGO.SetActive(false);
+            healthbar = healthBarGO.GetComponent<HealthBar>();
+            Transform canvas = this.gameObject.transform.GetChild(0);
+            healthBarGO.transform.SetParent(canvas, false);
+        }
+    }
     public void FadeAway()
     {
 
